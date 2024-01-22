f9a1ed78321b66e32e0add0538dd65dc56928631
sekizlipenguen <muratdemir3475@gmail.com>
2024-01-22 23:47:31 +0300

    typescript index #jrdn91

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,68 @@
-import Root from './src/main/Root';
-import Popup from './src/main/Popup';
-import Toast from './src/main/Toast';
-import SPSheet from './src/main/SPSheet';
-import {getStatusBarHeight} from './src/main/Helper';
+declare module "react-native-popup-confirm-toast" {
+    import {FC} from "react"
+    import {StyleProp, TextStyle, ViewStyle} from "react-native"
+    import {IconProps} from "react-native-vector-icons/Icon"
 
-declare module 'react-native-popup-confirm-toast' {
-    // @ts-ignore
-    export {
-        Root,
-        Popup,
-        SPSheet,
-        Toast,
-        getStatusBarHeight
+    export interface Config {
+        title?: string
+        text?: string
+        titleTextStyle?: StyleProp<TextStyle>
+        descTextStyle?: StyleProp<TextStyle>
+        backgroundColor?: string
+        timeColor?: string
+        position?: "top" | "bottom"
+        icon?: FC<IconProps>
+        timing?: number
+        statusBarType?: "default" | "dark-content" | "light-content"
+        statusBarTranslucent?: boolean
+        statusBarHidden?: boolean
+        statusBarAndroidHidden?: boolean
+        statusBarAppleHidden?: boolean
+        hiddenDuration?: number
+        startDuration?: number
+        onOpen?: () => void
+        onOpenComplete?: () => void
+        onClose?: () => void
+        onCloseComplete?: () => void
     }
+
+    export interface SheetConfig {
+        background?: string
+        height?: number
+        duration?: number
+        closeDuration?: number
+        closeOnDragDown?: boolean
+        closeOnPressMask?: boolean
+        closeOnPressBack?: boolean
+        dragTopOnly?: boolean
+        component?: FC
+        onOpen?: () => void
+        onOpenComplete?: () => void
+        onClose?: () => void
+        onCloseComplete?: () => void
+        customStyles?: StyleProp<ViewStyle>
+        timing?: number
+        keyboardHeightAdjustment?: boolean
+    }
+
+    export interface Toast {
+        show: (props: Config) => void
+        hide: () => void
+    }
+
+    export interface Popup {
+        show: (props: Config) => void
+        hide: () => void
+    }
+
+    export interface SPSheet {
+        show: (props: SheetConfig) => void
+        setHeight: (height: number, onOpenComplete: () => void) => void
+        hide: () => void
+    }
+
+    export const Toast: Toast
+    export const Popup: Popup
+    export const SPSheet: SPSheet
+    export const Root: FC
 }
