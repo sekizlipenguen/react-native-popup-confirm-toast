2c7a2a72c7d1646a5f45db06b1b78e0f5ce4b506
sekizlipenguen <muratdemir3475@gmail.com>
2025-05-30 16:59:05 +0300

    Add app state and dimensions change listeners for cleanup logic
    Integrated listeners for `AppState` and `Dimensions` to handle popup, toast, and sheet cleanup when the app state changes or screen size indicates potential PIP mode. Ensured proper removal of listeners during unmount.

--- src/main/Root.js
+++ src/main/Root.js
@@ -1,48 +1,95 @@
 import React, {Component} from 'react';
 
+import {AppState, Dimensions} from 'react-native';
 import PropTypes from 'prop-types';
 import Popup from './Popup';
 import Toast from './Toast';
 import SPSheet from './SPSheet';
 
 class Root extends Component {
-    render() {
-        return (
-            <>
-                {this.props.children}
-                <Popup
-                    ref={c => {
-                        if (c) {
-                            Popup.popupInstance = c;
-                        }
-                    }}
-                />
+  constructor(props) {
+    super(props);
+    this.handleAppStateChange = this.handleAppStateChange.bind(this);
+    this.handleDimensionsChange = this.handleDimensionsChange.bind(this);
+  }
 
-                <Toast
-                    ref={c => {
-                        if (c) {
-                            Toast.toastInstance = c;
-                        }
-                    }}
-                />
-                <SPSheet
-                    ref={c => {
-                        if (c) {
-                            SPSheet.spsheetInstance = c;
-                        }
-                    }}
-                />
-            </>
-        );
+  componentDidMount() {
+    this.appStateSubscription = AppState.addEventListener('change', this.handleAppStateChange);
+    this.dimensionsSubscription = Dimensions.addEventListener('change', this.handleDimensionsChange);
+  }
+
+  componentWillUnmount() {
+    this.appStateSubscription?.remove();
+    this.dimensionsSubscription?.remove();
+  }
+
+  handleAppStateChange(nextAppState) {
+    // Uygulama arka plana gittiğinde veya PIP moduna geçtiğinde
+    if (nextAppState === 'background' || nextAppState === 'inactive') {
+      this.cleanupAllPopups();
     }
+  }
+
+  handleDimensionsChange() {
+    // Ekran boyutu değiştiğinde
+    const {width, height} = Dimensions.get('screen');
+    // Ekran küçüldüyse, PIP moduna geçilmiş olabilir
+    if (width < 300 || height < 300) {
+      this.cleanupAllPopups();
+    }
+  }
+
+  cleanupAllPopups() {
+    // Tüm açık popupları, toastları ve sheetleri kapat
+    if (Popup.popupInstance) {
+      Popup.hide();
+    }
+    if (Toast.toastInstance) {
+      Toast.hide();
+    }
+    if (SPSheet.spsheetInstance) {
+      SPSheet.hide();
+    }
+  }
+
+  render() {
+    return (
+        <>
+          {this.props.children}
+          <Popup
+              ref={c => {
+                if (c) {
+                  Popup.popupInstance = c;
+                }
+              }}
+          />
+
+          <Toast
+              ref={c => {
+                if (c) {
+                  Toast.toastInstance = c;
+                }
+              }}
+          />
+          <SPSheet
+              ref={c => {
+                if (c) {
+                  SPSheet.spsheetInstance = c;
+                }
+              }}
+          />
+        </>
+    );
+  }
 }
 
 Root.propTypes = {
-    style: PropTypes.oneOfType([
-        PropTypes.object,
-        PropTypes.number,
-        PropTypes.array
-    ])
+  style: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.number,
+    PropTypes.array,
+  ]),
 }
 
 export default Root
+
